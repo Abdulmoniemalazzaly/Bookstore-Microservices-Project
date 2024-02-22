d82724a6706afea57a0dbe39795412d0e2f14351
Abdulmoniem <abdulmoniem.alazzaly@gmail.com>
2024-02-22 10:56:09 +0200

    Delete Commons directory

--- Commons/src/main/java/com/bookstore/commons/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.commons.exception;
-
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public class UserNotFoundException extends UsernameNotFoundException {
-    public UserNotFoundException(String msg) {
-        super(msg);
-    }
-}
